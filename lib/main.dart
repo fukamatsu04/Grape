40387782f6f6065b6809f5739f023208201cc04b
Isaac Silva <isaackun98@gmail.com>
2022-05-07 13:26:30 -0300

    Config Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login/home_screen.dart';
 import 'package:login/sing_up_screen.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(LogIn());
 }
 
